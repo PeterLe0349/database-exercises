8c55274a609a75e0028a9f868a85cb94df9112a5
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-07-07 15:02:10 -0500

    update

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,5 +1,6 @@
 use employees;
 select 'EMPLOYEES with Irena, Maya, Vidya' as 'INFO';
+# select * from employees where first_name in ( 'Irena','Maya','Vidya') AND gender = 'M';
 select * from employees where (first_name = 'Irena' OR first_name = 'Maya' OR first_name = 'Vidya') AND gender = 'M';
 select 'EMPLOYEES with Last name starting with E' as 'INFO';
 select * from employees where last_name like 'E%' AND last_name like '%E';
